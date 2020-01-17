ba2b2de1b6f886e20cc24aa744ad25a0e693a644
Max Harlow <max.harlow@ft.com>
2020-01-17 11:44:26 +0000

    Updates

--- members-financial-interests.js
+++ members-financial-interests.js
@@ -1,21 +1,51 @@
-const Highland = require('highland')
-const Request = require('request')
-const RetryMe = require('retry-me')
+const Crypto = require('crypto')
+const Ix = require('ix')
+const Axios = require('axios')
+const AxiosRetry = require('axios-retry')
 const Cheerio = require('cheerio')
-const FS = require('fs')
-const CSVWriter = require('csv-write-stream')
+const PapaParse = require('papaparse')
+const FSExtra = require('fs-extra')
 
-const http = Highland.wrapCallback((location, callback) => {
-    const wrapper = location => {
-        return callbackInner => {
-            Request.defaults({ timeout: 30 * 1000 })(location, (error, response) => {
-                const failure = error ? error : response.statusCode >= 400 ? new Error(response.statusCode + ' for ' + response.request.href) : null
-                callbackInner(failure, response)
-            })
+async function request(location) {
+    const url = typeof location === 'object' ? location.url : location
+    const hash = Crypto.createHash('sha1').update(url).digest('hex')
+    const useCache = location.useCache !== undefined ? location.useCache : true
+    const isCached = await FSExtra.pathExists(`cache/${hash}`)
+    if (isCached && useCache) {
+        console.log(`Cached: ${url}...`)
+        const cache = await FSExtra.readFile(`cache/${hash}`)
+        return {
+            url,
+            data: JSON.parse(cache),
+            passthrough: location.passthrough
         }
     }
-    RetryMe(wrapper(location), { factor: 1.5 }, callback)
-})
+    console.log(`Requesting: ${url}...`)
+    const timeout = 30 * 1000
+    const instance = Axios.create({ timeout })
+    AxiosRetry(instance, {
+        retries: 10,
+        shouldResetTimeout: true,
+        retryCondition: e => {
+            return !e.response || e.response.status >= 500 // no response or server error
+        },
+        retryDelay: (number, e) => {
+            if (number === 1) console.log(`Received ${(e.response && e.response.status) || e.code}: ${e.config.url}`)
+            else console.log(`  → Received ${e.response.status || e.code} in retry attempt #${number - 1}: ${e.config.url}`)
+            return 5 * 1000
+        }
+    })
+    const response = await instance(location)
+    if (useCache) {
+        await FSExtra.ensureDir('cache')
+        await FSExtra.writeJson(`cache/${hash}`, response.data)
+    }
+    return {
+        url,
+        data: response.data,
+        passthrough: location.passthrough
+    }
+}
 
 function range(from, to) {
     return [...Array(to - from + 1)].map((_, i) => from + i)
@@ -25,37 +55,48 @@ function locations() {
     const years = range(10, new Date().getFullYear() - 2000)
     const months = range(1, 12).map(n => n < 10 ? '0' + n: '' + n)
     const days = range(1, 31).map(n => n < 10 ? '0' + n: '' + n)
-    const keys = years.map(year => months.map(month => days.map(day => year + month + day))).join(',').split(',').filter(key => {
+    const editions = years.map(year => months.map(month => days.map(day => year + month + day))).join(',').split(',').filter(edition => {
         const firstPossibleEntry = 100525 // state opening of the 2010-12 parliament
         const lastPossibleEntry = Number(new Date().toISOString().substr(2, 8).replace(/-/g, '')) // today
-        const keyNumber = Number(key)
-        return keyNumber >= firstPossibleEntry && keyNumber <= lastPossibleEntry
+        const editionNumber = Number(edition)
+        return editionNumber >= firstPossibleEntry && editionNumber <= lastPossibleEntry
     })
-    return keys.map(key => {
-        const title = key > 151214 ? 'contents' : 'part1contents' // title changed on this date
-        return `http://www.publications.parliament.uk/pa/cm/cmregmem/${key}/${title}.htm`
+    return editions.map(edition => {
+        const title = edition > 151214 ? 'contents' : 'part1contents' // title changed on this date
+        const url = `http://www.publications.parliament.uk/pa/cm/cmregmem/${edition}/${title}.htm`
+        return {
+            url,
+            passthrough: {
+                url,
+                edition
+            }
+        }
     })
 }
 
 function members(response) {
-    const document = Cheerio.load(response.body)
+    const document = Cheerio.load(response.data)
     if (document('h1').text().trim() === 'Page cannot be found') return []
-    console.log('Processing register ' + response.request.href.split('/')[6] + '...')
+    console.log(`Processing register ${response.passthrough.edition}...`)
     return document('td > p > a[href$=htm], #mainTextBlock > p > a[href$=htm]').not('[href="introduction.htm"]').get().map(entry => {
-        const base = response.request.href.replace('part1contents.htm', '').replace('contents.htm', '')
-        return base + Cheerio(entry).attr('href')
+        const base = response.passthrough.url.replace('part1contents.htm', '').replace('contents.htm', '')
+        return {
+            url: base + encodeURIComponent(Cheerio(entry).attr('href')),
+            passthrough: {
+                edition: response.passthrough.edition
+            }
+        }
     })
 }
 
 function contents(response) {
-    const document = Cheerio.load(response.body)
+    const document = Cheerio.load(response.data)
     if (document('td p').text().indexOf('Nil') >= 0) return []
     const name = document('h2').text().trim().split(' (')[0]
-    const edition = response.request.href.split('/')[6]
     const headings = document('td > h3, td > strong, td > p:has(strong), #mainTextBlock > h3, td > strong, #mainTextBlock > p:has(strong)').get().filter(heading => {
         return Cheerio(heading).text().trim().match(/^\d{1,2}\./) // filter out those that look like a heading but aren't
     })
-    return headings.map((heading, i) => {
+    return headings.flatMap((heading, i) => {
         const nextHeading = i === headings.length ? 'div' : headings[i + 1] // if this heading is the last, look for a <div> signifying the end
         const items = Cheerio(heading).nextUntil(nextHeading).get().reduce((a, item) => {
             const block = Cheerio(item)
@@ -81,8 +122,8 @@ function contents(response) {
             const registeredMatch = item.match(/\((:?Registered)?(:? )*(\d{1,2} \S+ \d{4})/)
             return {
                 name,
-                editionDeclared: edition,
-                editionLastSeen: edition,
+                editionDeclared: response.passthrough.edition,
+                editionLastSeen: response.passthrough.edition,
                 section: Cheerio(heading).text().trim(),
                 item,
                 amount: amountMatch ? amountMatch[0] : '',
@@ -99,12 +140,14 @@ function valid(text) {
         && !text.match(/^(Donations to my constituency|Donations to the constituency|Donations to support|Support in the capacity as|Payments recieved in my capacity as|Other donations|Other support)/i)
 }
 
-var data = []
 
-function dedupe(row) {
-    const exists = data.find(existing => existing.name === row.name && existing.item === row.item && existing.editionDeclared !== row.editionDeclared)
-    if (exists) exists.editionLastSeen = row.editionLastSeen
-    else data.push(row)
+function dedupe(a, row) {
+    const current = a.find(existing => existing.name === row.name && existing.item === row.item && existing.editionDeclared !== row.editionDeclared)
+    if (current) {
+        current.editionLastSeen = row.editionLastSeen
+        return a
+    }
+    else return a.concat(row)
 }
 
 function alphachronological(a, b) {
@@ -117,18 +160,36 @@ function alphachronological(a, b) {
     return 0
 }
 
-Highland(locations())
-    .flatMap(http)
-    .flatMap(members)
-    .flatMap(http)
-    .flatMap(contents)
-    .flatten()
-    .map(dedupe)
-    .errors(e => console.error(e.stack))
-    .done(() => {
-        console.log('Sorting...')
-        Highland(data)
-            .sortBy(alphachronological)
-            .through(CSVWriter())
-            .pipe(FS.createWriteStream('members-financial-interests.csv'))
-    })
+function csv() {
+    let headerWritten = false
+    return function* (record) {
+        if (!headerWritten) {
+            const header = PapaParse.unparse([Object.keys(record)])
+            yield header + '\n'
+            headerWritten = true
+        }
+        const entry = PapaParse.unparse([Object.values(record)])
+        yield entry + '\n'
+    }
+}
+
+async function write(filename) {
+    await FSExtra.remove(filename)
+    return contents => FSExtra.appendFile(filename, contents)
+}
+
+async function run() {
+    const all = await Ix.AsyncIterable.from(locations())
+        .map(request)
+        .flatMap(members)
+        .map(request)
+        .flatMap(contents)
+        .reduce(dedupe, [])
+    const sorted = all.sort(alphachronological)
+    return Ix.AsyncIterable.from(sorted)
+        .flatMap(csv())
+        .forEach(await write('members-financial-interests.csv'))
+        .finally(() => console.log('Done!'))
+}
+
+run()
